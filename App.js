5dd201f616d6d0a5f6a12b3c0fe6813de5b6e0e1
bsb-github <codewithsheri64@gmail.com>
2023-10-01 14:45:45 +0500

    fixed port issue

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
    // res.sendStatus(403);
     });
 app.use("/todo", todoRoute);
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port "+PORT);
 });
